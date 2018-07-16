1a550248e944c2e533295b4d45ce24c6dded884c
Kevin Chi <kevinchi118@gmail.com>
2018-07-16 02:19:18 +0000

    automatically backed up by learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,16 @@
 class Pokemon
+    attr_accessor :name, :type, :db
+
+    @@all = []
+
+    initialize(name, type, db)
+    end
+
+    def self.save(name, type, db)
+        @@all << Pokemon.new(name, type, db)
+    end
+
+    def self.find
+    end
+
 end
